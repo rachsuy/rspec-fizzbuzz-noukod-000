05d25f4d2cd6b5262a904bb22663cc4b14fa8d54
Rachele Suy <rachelesuy@gmail.com>
2019-04-09 18:25:24 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(fizz_3)
+  
+ if fizz_3 % 3 == 0 && fizz_3 % 5 == 0
+     return "FizzBuzz"
+  elsif fizz_3 % 3 == 0 
+     return "Fizz"
+  elsif fizz_3 % 5 == 0
+     return "Buzz"
+   else
+     return nil
+end
+end
